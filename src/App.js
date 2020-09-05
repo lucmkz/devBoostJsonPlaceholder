5c2a50c4e0f733f06e7cae64fe4c0624a80d2ae3
Lucas Duarte <l.duarte.mk@gmail.com>
2020-09-04 21:56:16 -0300

    add api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
+import api from './services'
 import "./styles.css";
 
 export default function App() {
+
+  useEffect(() => {
+    api.get('/todos').then(r => console.log(r.data))
+  }, [])
+
   return (
     <div className="App">
       <h1>Hi (:</h1>
